fcd926216ef4a68274ebb3566fc81e83192ab6a3
officialrazzaer <132298394+officialrazzaer@users.noreply.github.com>
2023-08-03 17:22:06 +0100

    functioning responsive website with a calculator

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 "use strict";
+
+let calculation = "";
+const screen = document.getElementById("screen");
+
+function appendToScreen(value) {
+  calculation += value;
+  updateScreen();
+}
+
+function clearScreen() {
+  calculation = "";
+  updateScreen();
+}
+
+function calculateResult() {
+  try {
+    const result = eval(calculation);
+    calculation = result.toString();
+    updateScreen();
+  } catch (error) {
+    calculation = "Error";
+    updateScreen();
+  }
+}
+
+function updateScreen() {
+  screen.textContent = calculation;
+}
